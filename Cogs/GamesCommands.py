88de530b51396380142180f2f1ddf5eb60afda70
AlfredoFilho <alfredoalbelisb@gmail.com>
2021-10-20 20:27:20 -0300

    duck | 8ball | reverse from @AlexFlipnote https://github.com/AlexFlipnote/discord_bot.py/

--- Cogs/GamesCommands.py
+++ Cogs/GamesCommands.py
@@ -1,11 +1,24 @@
 #!/bin/bash/python3
 #coding: utf-8
 
+import sys
 import random
+import aiohttp
 import discord
 import asyncio
 from discord.ext import commands
 from discord_components import Button, ButtonStyle
+
+# Local import
+sys.path.append("..")
+from Utils import http
+
+"""
+Commands: duck, reverse and eightball from @AlexFlipnote
+
+https://github.com/AlexFlipnote
+https://github.com/AlexFlipnote/discord_bot.py/blob/master/cogs/fun.py
+"""
 
 
 class GamesCommands(commands.Cog):
@@ -96,6 +109,61 @@ class GamesCommands(commands.Cog):
             embedRps.clear_fields()
             embedRps.add_field(name="Time", value="`Time is over`")
             await embedRpsButton.edit(embed=embedRps, components=[])
+    
+    
+    @commands.command(aliases=["8ball"])
+    @commands.guild_only()
+    async def eightball(self, ctx, *, question: commands.clean_content):
+        """ Consult 8ball to receive an answer """
+        ballresponse = [
+            "Yes", "No", "Take a wild guess...", "Very doubtful",
+            "Sure", "Without a doubt", "Most likely", "Might be possible",
+            "You'll be the judge", "no... (╯°□°）╯︵ ┻━┻", "no... baka",
+            "senpai, pls no ;-;"
+        ]
+
+        answer = random.choice(ballresponse)
+        await ctx.send(f"🎱 **Question:** {question}\n**Answer:** {answer}")
+    
+
+    @eightball.error
+    async def eightballerror(self, ctx, error):
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You forgot the question.")
+            ctx.handled_in_local = True
+
+
+    async def randomimageapi(self, ctx, url: str, endpoint: str, token: str = None):
+        try:
+            r = await http.get(
+                url, res_method="json", no_cache=True,
+                headers={"Authorization": token} if token else None
+            )
+        except aiohttp.ClientConnectorError:
+            return await ctx.send("The API seems to be down...")
+        except aiohttp.ContentTypeError:
+            return await ctx.send("The API returned an error or didn't return JSON...")
+
+        await ctx.send(r[endpoint])
+
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
+    async def duck(self, ctx):
+        """ Posts a random duck """
+        await self.randomimageapi(ctx, "https://random-d.uk/api/v1/random", "url")
+
+    
+    @commands.command()
+    @commands.guild_only()
+    async def reverse(self, ctx, *, text: str):
+        """ !poow ,ffuts esreveR
+        Everything you type after reverse will of course, be reversed
+        """
+        t_rev = text[::-1].replace("@", "@\u200B").replace("&", "&\u200B")
+        await ctx.send(f"🔁 {t_rev}")
     
     
 def setup(bot):
