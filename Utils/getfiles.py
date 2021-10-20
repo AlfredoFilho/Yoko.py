7ae90e41b2a4c0b0799d99d8a271aebba345d6e8
AlfredoFilho <alfredoalbelisb@gmail.com>
2021-10-20 20:36:35 -0300

    rename file to lowercase

---
+++ Utils/getfiles.py
@@ -0,0 +1,19 @@
+#!/bin/bash/python3
+#coding: utf-8
+
+import json
+import pandas as pd
+
+
+def getJsonData(pathToJsonFile):
+
+    with open(pathToJsonFile) as json_data:
+        jsonData = json.load(json_data)
+
+    return jsonData
+
+
+def getDictFromCsv(pathToCsv, nameKey, nameValue):
+    
+    df = pd.read_csv(pathToCsv)
+    return pd.Series(df[nameValue].values,index=df[nameKey]).to_dict()
